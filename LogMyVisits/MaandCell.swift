430431f05a277848210b8e18d5307eade1a08072
Toine Schnabel <Toine.schnabel@gmail.com>
2018-09-18 17:43:54 +0200

    Maandcell toegevoegd

---
+++ LogMyVisits/MaandCell.swift
@@ -0,0 +1,33 @@
+//
+//  MaandCell.swift
+//  LogMyWay
+//
+//  Created by Toine Schnabel on 16-07-17.
+//  Copyright © 2017 Toine Schnabel. All rights reserved.
+//
+
+import UIKit
+
+class MaandCell: UITableViewCell {
+
+    @IBOutlet var jaar: UILabel!
+    @IBOutlet var maand: UILabel!
+    @IBOutlet var landen: UILabel!
+    @IBOutlet var selectedVoorKaart: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    func met(data:cellData){
+     jaar.text =  data.datum.yyyy
+     maand.text = data.datum.MMMM
+     landen.text = data.data[0]
+        if data.selected {selectedVoorKaart.text = "√"} else {selectedVoorKaart.text = ""}
+        selectedVoorKaart.isHidden = !data.selected
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
